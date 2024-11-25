b27e897e00835142d7b3a9d686f34a572f4f3404
Prateek Singh <prateeksingh@Prateeks-MacBook-Air-2.local>
2024-11-25 19:25:57 +0530

    Added autotagging for html

--- lua/user/plugins/plugins.lua
+++ lua/user/plugins/plugins.lua
@@ -130,7 +130,29 @@ require("lazy").setup({
 		-- Adds a beautiful line above command line
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = true	},
+		config = true
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		config = function()
+			require('nvim-ts-autotag').setup({
+				opts = {
+					-- Defaults
+					enable_close = true, -- Auto close tags
+					enable_rename = true, -- Auto rename pairs of tags
+					enable_close_on_slash = false -- Auto close on trailing </
+				},
+				-- Also override individual filetype configs, these take priority.
+				-- Empty by default, useful if one of the "opts" global settings
+				-- doesn't work well in a specific filetype
+				per_filetype = {
+					["html"] = {
+						enable_close = false
+					}
+				}
+			})
+		end
+	},
 	{
 		"ThePrimeagen/vim-be-good",
 	},
