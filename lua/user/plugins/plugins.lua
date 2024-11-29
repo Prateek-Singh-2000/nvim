7412ed82bfef9ee3a5e86c08a05dc9c5b13d4f3e
Prateek-Singh-2000 <prateekmt787@gmail.com>
2024-11-29 11:23:52 +0530

    Added custom mini animate config settings

--- lua/user/plugins/plugins.lua
+++ lua/user/plugins/plugins.lua
@@ -158,7 +158,29 @@ require("lazy").setup({
 				'echasnovski/mini.nvim',
 				version = false,
 				config = function()
-					require('mini.animate').setup()
+					require('mini.animate').setup({
+						-- Vertical scroll animation configuration
+						scroll = {
+							-- Enable scrolling animation
+							enable = true,
+
+							-- Customize animation timing
+							timing = require('mini.animate').gen_timing.linear({ 
+								duration = 100, 
+								unit = 'total' 
+							}),
+
+							-- Customize scroll generator
+							generator = function(total_scroll)
+								local config = {
+									unit = 'line',
+									-- Limit the scroll amount to reduce visual noise
+									total = math.min(total_scroll, 20)
+								}
+								return require('mini.animate').gen_scroll.line(config)
+							end
+						}
+					})
 				end
 			},
 			{
