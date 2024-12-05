211430b485991f37415ae40ee39fb81822828fcd
Prateek-Singh-2000 <prateekmt787@gmail.com>
2024-12-05 14:43:52 +0530

    Added midnight theme plugin

--- lua/user/plugins/plugins.lua
+++ lua/user/plugins/plugins.lua
@@ -17,152 +17,158 @@ require("lazy").setup({
 	{
 		"rebelot/kanagawa.nvim",
 		-- config = function()
-			-- 	vim.cmd.colorscheme("kanagawa-dragon")
-			-- end
+		-- 	vim.cmd.colorscheme("kanagawa-dragon")
+		-- end
+	},
+	{
+		"dasupradyumna/midnight.nvim",
+		config = function()
+			vim.cmd.colorscheme("midnight")
+		end
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+		-- config = function()
+		-- 	vim.cmd.colorscheme("tokyonight-night")
+		-- end
+	},
+	{
+		"rafi/awesome-vim-colorschemes",
+		-- lazy = false,
+		-- priority = 1000,
+		-- opts = {},
+		-- config = function()
+		-- 	vim.cmd.colorscheme("twilight256")
+		-- end
+	},
+	{
+		-- Automatically pairs brackets, inverted commas, etc.
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end
+	},
+	{
+		-- Plugin to autocomment in Neovim
+		'numToStr/Comment.nvim',
+		opts = {
+			---Add a space b/w comment and the line
+			padding = true,
+			---Whether the cursor should stay at its position
+			sticky = true,
+			---Lines to be ignored while (un)comment
+			ignore = nil,
+			---LHS of toggle mappings in NORMAL mode
+			toggler = {
+				---Line-comment toggle keymap
+				line = 'gcc',
+				---Block-comment toggle keymap
+				block = 'gbc',
+			},
+			---Enable keybindings
+			---NOTE: If given `false` then the plugin won't create any mappings
+			mappings = {
+				---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+				basic = true,
+				---Extra mapping; `gco`, `gcO`, `gcA`
+				extra = true,
+			},
+			---Function to call before (un)comment
+			pre_hook = nil,
+			---Function to call after (un)comment
+			post_hook = nil,
+		}
+	},
+	{
+		"neovim/nvim-lspconfig"
+	},
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("mason-lspconfig").setup({})
+			require("mason-lspconfig").setup_handlers {
+				function(server_name)
+					require("lspconfig")[server_name].setup {}
+				end,
+			}
+		end,
+	},
+	{
+		"nvim-tree/nvim-web-devicons",
+	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
 		},
-		{
-			"folke/tokyonight.nvim",
-			lazy = false,
-			priority = 1000,
-			opts = {},
-			-- config = function()
-				-- 	vim.cmd.colorscheme("tokyonight-night")
-				-- end
-			},
-			{
-				"rafi/awesome-vim-colorschemes",
-				-- lazy = false,
-				-- priority = 1000,
-				-- opts = {},
-				config = function()
-					vim.cmd.colorscheme("twilight256")
-				end
-			},
-			{
-				-- Automatically pairs brackets, inverted commas, etc.
-				"windwp/nvim-autopairs",
-				event = "InsertEnter",
-				config = function()
-					require("nvim-autopairs").setup({})
-				end
-			},
-			{
-				-- Plugin to autocomment in Neovim
-				'numToStr/Comment.nvim',
+		config = function()
+			require("user.plugins.nvim-tree-config")
+		end,
+	},
+	{
+		"nvimtools/none-ls.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		main = "ibl",
+		opts = {
+			indent = { char = "┊" },
+		},
+	},
+	{
+		-- Adds a beautiful line above command line
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = true
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		config = function()
+			require('nvim-ts-autotag').setup({
 				opts = {
-					---Add a space b/w comment and the line
-					padding = true,
-					---Whether the cursor should stay at its position
-					sticky = true,
-					---Lines to be ignored while (un)comment
-					ignore = nil,
-					---LHS of toggle mappings in NORMAL mode
-					toggler = {
-						---Line-comment toggle keymap
-						line = 'gcc',
-						---Block-comment toggle keymap
-						block = 'gbc',
-					},
-					---Enable keybindings
-					---NOTE: If given `false` then the plugin won't create any mappings
-					mappings = {
-						---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
-						basic = true,
-						---Extra mapping; `gco`, `gcO`, `gcA`
-						extra = true,
-					},
-					---Function to call before (un)comment
-					pre_hook = nil,
-					---Function to call after (un)comment
-					post_hook = nil,
-				}
-			},
-			{
-				"neovim/nvim-lspconfig"
-			},
-			{
-				"williamboman/mason.nvim",
-				config = function()
-					require("mason").setup({})
-				end,
-			},
-			{
-				"williamboman/mason-lspconfig.nvim",
-				dependencies = { "williamboman/mason.nvim" },
-				config = function()
-					require("mason-lspconfig").setup({})
-					require("mason-lspconfig").setup_handlers {
-						function(server_name)
-							require("lspconfig")[server_name].setup {}
-						end,
+					-- Defaults
+					enable_close = true, -- Auto close tags
+					enable_rename = true, -- Auto rename pairs of tags
+					enable_close_on_slash = false -- Auto close on trailing </
+				},
+				-- Also override individual filetype configs, these take priority.
+				-- Empty by default, useful if one of the "opts" global settings
+				-- doesn't work well in a specific filetype
+				per_filetype = {
+					["html"] = {
+						enable_close = false
 					}
-				end,
-			},
-			{
-				"nvim-tree/nvim-web-devicons",
-			},
-			{
-				"nvim-tree/nvim-tree.lua",
-				version = "*",
-				lazy = false,
-				dependencies = {
-					"nvim-tree/nvim-web-devicons",
-				},
-				config = function()
-					require("user.plugins.nvim-tree-config")
-				end,
-			},
-			{
-				"nvimtools/none-ls.nvim",
-				dependencies = {
-					"nvim-lua/plenary.nvim",
-				},
-			},
-			{
-				"lukas-reineke/indent-blankline.nvim",
-				event = { "BufReadPre", "BufNewFile" },
-				main = "ibl",
-				opts = {
-					indent = { char = "┊" },
-				},
-			},
-			{
-				-- Adds a beautiful line above command line
-				"nvim-lualine/lualine.nvim",
-				dependencies = { "nvim-tree/nvim-web-devicons" },
-				config = true
-			},
-			{
-				"windwp/nvim-ts-autotag",
-				config = function()
-					require('nvim-ts-autotag').setup({
-						opts = {
-							-- Defaults
-							enable_close = true, -- Auto close tags
-							enable_rename = true, -- Auto rename pairs of tags
-							enable_close_on_slash = false -- Auto close on trailing </
-						},
-						-- Also override individual filetype configs, these take priority.
-						-- Empty by default, useful if one of the "opts" global settings
-						-- doesn't work well in a specific filetype
-						per_filetype = {
-							["html"] = {
-								enable_close = false
-							}
-						}
-					})
-				end
-			},
-			{
-				"kshenoy/vim-signature",
-			},
-			{
-				"ThePrimeagen/vim-be-good",
-			},
-			require("user.plugins.formatting"),
-			require("user.plugins.linters.sonarlint"),
-			require("user.plugins.telescope"),
-			require("user.plugins.treesitter"),
-			require("user.plugins.nvim-cmp"),
-			-- require("user.plugins.scrollers.neoscroll"),
-		})
+				}
+			})
+		end
+	},
+	{
+		"kshenoy/vim-signature",
+	},
+	{
+		"ThePrimeagen/vim-be-good",
+	},
+	require("user.plugins.formatting"),
+	require("user.plugins.linters.sonarlint"),
+	require("user.plugins.telescope"),
+	require("user.plugins.treesitter"),
+	require("user.plugins.nvim-cmp"),
+	-- require("user.plugins.scrollers.neoscroll"),
+})
