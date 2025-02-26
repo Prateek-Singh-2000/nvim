6150fe5893098266aa6537096c1ea61816b49f49
Prateek-Singh-2000 <prateekmt787@gmail.com>
2025-02-26 11:24:16 +0530

    Added pyright server lspconfig

--- lua/user/plugins/linters/sonarlint.lua
+++ lua/user/plugins/linters/sonarlint.lua
@@ -8,6 +8,7 @@ return {
 		-- Register the SonarLint config
 		local lspconfig = require('lspconfig')
 		lspconfig.cssls.setup {}
+		lspconfig.pyright.setup {}
 		lspconfig.lua_ls.setup {}
 		lspconfig.html.setup {}
 		lspconfig.ts_ls.setup {}
