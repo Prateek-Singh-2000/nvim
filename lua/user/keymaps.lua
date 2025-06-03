4cdb00bdafb81e0b9a667c4c8b01b2b11a6d2233
Prateek-Singh-2000 <prateekmt787@gmail.com>
2025-06-03 15:16:51 +0530

    Removed keymap collision with harpoon and nohlsearch

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,5 +1,5 @@
 -- This file contains all keymap settings
-vim.keymap.set("n", "<Leader>h", ":nohlsearch<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>x", ":nohlsearch<CR>", { noremap = true, silent = true })
 
 
 -- Toggles nvim-tree to open and close
