3ca79f2268bf75bc85276443a9c93aedb58c79d7
Prateek-Singh-2000 <prateekmt787@gmail.com>
2025-07-21 16:15:48 +0530

    Added keymap to remove all buffer except currect

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -15,3 +15,13 @@ vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", { noremap = true })
 vim.keymap.set('n', '<leader>dd', '"_dd', { desc = 'Delete line without yank (blackhole)', noremap = true, silent = true })
 vim.keymap.set('v', '<leader>d', '"_d',
 	{ desc = 'Delete visual lines without yank (blackhole)', noremap = true, silent = true })
+
+-- Keymap to delete all buffers except the current one
+vim.keymap.set("n", "<leader>bd", function()
+  local current = vim.fn.expand("%")
+  for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_buf_is_loaded(bufnr) and vim.api.nvim_buf_get_name(bufnr) ~= current then
+      vim.api.nvim_buf_delete(bufnr, {})
+    end
+  end
+end, { desc = "Delete all buffers except current" })
