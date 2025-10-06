647453121e838f329f6d32c42524f556fe097e02
Prateek-Singh-2000 <prateekmt787@gmail.com>
2025-10-06 15:14:55 +0530

    Added keybind for monitor for centerpad

--- lua/user/plugins/buffer_alignment/centerpad.lua
+++ lua/user/plugins/buffer_alignment/centerpad.lua
@@ -2,7 +2,10 @@ return {
 	'smithbm2316/centerpad.nvim',
 	config = function()
 		vim.keymap.set("n", "<leader>cc", function()
-			require("centerpad").toggle { leftpad = 20, rightpad = 0 }
+			require("centerpad").toggle { leftpad = 25, rightpad = 0 }
+		end, { noremap = true, silent = true, desc = "Toggle Centerpad" })
+		vim.keymap.set("n", "<leader>cv", function()
+			require("centerpad").toggle { leftpad = 45, rightpad = 20 }
 		end, { noremap = true, silent = true, desc = "Toggle Centerpad" })
 	end,
 }
