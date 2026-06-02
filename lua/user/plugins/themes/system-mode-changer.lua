8911995590be17ba5110ab6c11d0d0d7f44c6af3
Prateek-Singh-2000 <prateekmt787@gmail.com>
2026-06-02 13:32:44 +0530

    using nightfox theme

--- lua/user/plugins/themes/system-mode-changer.lua
+++ lua/user/plugins/themes/system-mode-changer.lua
@@ -3,7 +3,7 @@ return {
 	opts = {
 		set_dark_mode = function()
 			vim.api.nvim_set_option_value("background", "dark", {})
-			vim.cmd('colorscheme terafox')
+			vim.cmd('colorscheme nightfox')
 		end,
 		set_light_mode = function()
 			vim.api.nvim_set_option_value("background", "light", {})
