d9d4ce5c055c6443b0a3267d2c13f0983f64bf41
Prateek-Singh-2000 <prateekmt787@gmail.com>
2026-06-18 16:58:07 +0530

    created a function to maximize a neovim split

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,14 +1,9 @@
--- This file contains all keymap settings
 vim.keymap.set("n", "<Leader>x", ":nohlsearch<CR>", { noremap = true, silent = true })
 
-
--- Toggles nvim-tree to open and close
 vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 
--- Keymap to autoindent, is more convenient
 vim.api.nvim_set_keymap("n", "==", "gg=G", { noremap = true })
 
--- half page up/down remapping to always make cursor centered
 vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", { noremap = true })
 
@@ -17,12 +12,9 @@ vim.keymap.set('v', '<leader>d', '"_d',
 	{ desc = 'Delete visual lines without yank (blackhole)', noremap = true, silent = true })
 vim.keymap.set("n", "x", '"_x', { desc = 'Delete character without yank (blackhole)', noremap = true, silent = true })
 
--- Remap Ctrl-f to scroll half page up (like Ctrl-u)
 vim.keymap.set("n", "<C-f>", "<C-u>", { desc = "Scroll half page up" })
 vim.keymap.set("v", "<C-f>", "<C-u>", { desc = "Scroll half page up" })
 
--- Terminal mode keymaps
--- Enter terminal mode easily
 local term_buf = nil
 
 local function toggle_terminal()
@@ -41,3 +33,22 @@ end
 
 vim.keymap.set('n', '<leader>t', toggle_terminal)
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
+
+
+-- Toggle current split to full screen (new tab) and back
+local function toggle_maximize()
+  if vim.fn.tabpagenr('$') == 1 and #vim.fn.tabpagebuflist() == 1 then
+    return
+  end
+
+  local cur_win = vim.fn.winnr()
+  if vim.fn.tabpagenr('$') > 1 then
+    vim.cmd('tabclose')
+    vim.cmd(cur_win .. 'wincmd w')
+  else
+    vim.cmd('tab split')
+  end
+end
+
+vim.keymap.set('n', '<leader>m', toggle_maximize, { desc = "Toggle maximize split" })
+
